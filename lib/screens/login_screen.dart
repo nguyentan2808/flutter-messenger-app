21abc4adce5db902bbefe43b39d76ad4996d5d4e
nguyentan2808 <tanpro01635147801@gmail.com>
2022-04-09 17:32:47 +0700

    fix: login page overflow bottom

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,73 +10,66 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    print(size);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: SvgPicture.asset(
-                    'assets/images/login_svg.svg',
+      body: ListView(
+        children: [
+          SizedBox(
+            height: size.height * 0.45,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: SvgPicture.asset(
+                'assets/images/login_svg.svg',
+                alignment: Alignment.topCenter,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  "Login Details",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: "Outfit"),
+                ),
+                const SizedBox(height: 16),
+                const TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: const OutlineInputBorder(),
                   ),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Text(
-                      "Login Details",
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: "Outfit"),
-                    ),
-                    const SizedBox(height: 16),
-                    const TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Username',
-                        border: const OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Container(
-                      alignment: Alignment.topRight,
-                      child: const Text(
-                        "Forgot Password ?",
-                        style: TextStyle(),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(40))),
-                            fixedSize: MaterialStateProperty.all(
-                                Size(size.width * 0.9, 50))),
-                        onPressed: () {},
-                        child: const Text("Login")),
-                  ],
+                const SizedBox(height: 16),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
+                const SizedBox(height: 8),
+                Container(
+                  alignment: Alignment.topRight,
+                  child: const Text(
+                    "Forgot Password ?",
+                    style: TextStyle(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40))),
+                        fixedSize: MaterialStateProperty.all(
+                            Size(size.width * 0.9, 50))),
+                    onPressed: () {},
+                    child: const Text("Login")),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
