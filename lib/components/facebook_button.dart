ae9350eb03714f837715dc6a49dd67845ea9bdf5
nguyentan2808 <tanpro01635147801@gmail.com>
2022-05-01 04:12:16 +0700

    feat: facebook button

---
+++ lib/components/facebook_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class FaceBookButton extends StatelessWidget {
+  const FaceBookButton({
+    Key? key,
+    required this.text,
+    required this.icon,
+    required this.isActive,
+  }) : super(key: key);
+
+  final String text;
+  final IconData icon;
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = isActive ? Colors.white : Colors.black;
+
+    return MaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      padding: const EdgeInsets.all(10),
+      elevation: 0,
+      color: isActive ? Theme.of(context).primaryColor : Colors.grey[200],
+      onPressed: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 20, color: color),
+          const SizedBox(width: kDefaultPadding / 2),
+          Text(
+            text,
+            style: TextStyle(fontWeight: FontWeight.w600, color: color),
+          ),
+        ],
+      ),
+    );
+  }
+}
